e71700928632e5d0e311442680a44b555f04c970
JASPER BO <jbo@royal-11.cs.wisc.edu>
2023-10-31 16:55:00 -0500

    created new file for mmap system call

---
+++ xv6-public/mmap.c
@@ -0,0 +1,13 @@
+#include "param.h"
+#include "types.h"
+#include "defs.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "elf.h"
+#include "mmap.h"
+
+void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) {
+    
+}
